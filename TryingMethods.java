8f455e08eac4f4f6035c6c56d5135d24d5a6ba82
gmatolo <mulimatolo@gmail.com>
2022-10-27 16:25:13 +0000

    Adds the functions that uses switch to return day of the week

--- TryingMethods.java
+++ TryingMethods.java
@@ -3,4 +3,24 @@ public class TryingMethods {
         System.out.println();
 
     }
+
+    public static void weekDay(String[] args) {
+        int weekday = 0;
+        if (weekday == 0) {
+            System.out.println("Sunday");
+        } else if (weekday == 1) {
+            System.out.println("Monday");
+        } else if (weekday == 2) {
+            System.out.println("Tuesday");
+        } else if (weekday == 3) {
+            System.out.println("Wednesday");
+        } else if (weekday == 4) {
+            System.out.println("Thurday");
+        } else if (weekday == 5) {
+            System.out.println("Friday");
+        } else if (weekday == 6) {
+            System.out.println("Saturday");
+        }
+    }
+
 }
