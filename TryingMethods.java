d67ce621b86f3ca690ce3d87662200f1e870b0d8
gmatolo <mulimatolo@gmail.com>
2022-10-27 17:15:42 +0000

    Adds login feature that provides login status

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,6 +1,20 @@
 public class TryingMethods {
     public static void main(String[] args) {
         System.out.println();
+    }
+
+    // A function that thats in the username and function and prints the login status based on the user input
+    void login(String Username, String Password) {
+
+        if (Username == "GERALD.MATOLO" && Password == "G3r@!d") {
+            System.out.println("Login Successful");
+        } else if (Username == "GERALD.MATOLO" && Password != "G3r@!d") {
+            System.out.println("You Entered the wrong Password. Please Try Again!");
+        } else if (Username != "GERALD.MATOLO" && Password == "G3r@!d") {
+            System.out.println("You Entered the wrong Username. Please Try Again!");
+        } else (Username != "GERALD.MATOLO" && Password != "G3r@!d") {
+            System.out.println("You Entered the wrong Username and Password. Please Try Again!");
+        }
 
     }
 }
