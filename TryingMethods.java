5bd3a5e2028aa77ff626bff938beec40c8b1c2c7
gmatolo <mulimatolo@gmail.com>
2022-10-27 16:29:07 +0000

    solves bug for day of the week calculator

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,25 +1,39 @@
+
 public class TryingMethods {
     public static void main(String[] args) {
         System.out.println();
 
     }
 
-    public static void weekDay(String[] args) {
-        int weekday = 0;
-        if (weekday == 0) {
-            System.out.println("Sunday");
-        } else if (weekday == 1) {
-            System.out.println("Monday");
-        } else if (weekday == 2) {
-            System.out.println("Tuesday");
-        } else if (weekday == 3) {
-            System.out.println("Wednesday");
-        } else if (weekday == 4) {
-            System.out.println("Thurday");
-        } else if (weekday == 5) {
-            System.out.println("Friday");
-        } else if (weekday == 6) {
-            System.out.println("Saturday");
+    // A dynamic method that takes an integer as an argument.
+    // It returns a string of the day of the week based on the argument that was
+    // provided.
+    void dayOfTheWeek(int args) {
+        int day = 0;
+        switch (day) {
+            case 0:
+                System.out.println("The day of the week is: Sunday");
+                break;
+            case 1:
+                System.out.println("The day of the week is: Monday");
+                break;
+            case 2:
+                System.out.println("The day of the week is: Tuesday");
+                break;
+            case 3:
+                System.out.println("The day of the week is: Wednesday");
+                break;
+            case 4:
+                System.out.println("The day of the week is: Thursday");
+                break;
+            case 5:
+                System.out.println("The day of the week is: Friday");
+                break;
+            case 6:
+                System.out.println("The day of the week is: Saturday");
+                break;
+            default:
+                System.out.println("Day Not found");
         }
     }
 
