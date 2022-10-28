4af9f2a6390a326bed72e438d5015133996031f8
gmatolo <mulimatolo@gmail.com>
2022-10-28 06:28:52 +0000

    Added a static methods that returns the sum of all natural numbers between 0 and 1000

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,6 +1,24 @@
 public class TryingMethods {
     public static void main(String[] args) {
+
         System.out.println();
+        sumOfNaturalNumberLessThan1000();
 
     }
+
+    public static int sumOfNaturalNumberLessThan1000() {
+        int result = 0;
+        int num = 0;
+
+        while (num <= 1000) {
+            result += num;
+            num += 1;
+        }
+
+        System.out.println("The result is: " + result);
+
+        return result;
+
+    }
+
 }
